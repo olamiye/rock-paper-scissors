954300e59600cb71b5584c209209b149173547a7
Joseph Olamiye <captainjoe550@gmail.com>
2022-08-15 10:33:11 +0100

    starting out

---
+++ index.js
@@ -0,0 +1,17 @@
+let computerChoice = document.getElementById("computer-choice")
+let userChoice = document.getElementById("user-choice")
+let resultDisplay = document.getElementById("result")
+
+function userChoiceDisplay(){
+    userChoice.textContent = "rock"
+}
+
+function userChoiceDisplay2(){
+    userChoice.textContent = "paper"
+}
+
+function userChoiceDisplay3(){
+    userChoice.textContent = "scissors"
+}
+
+
